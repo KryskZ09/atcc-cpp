a1ac151dda7f6e6cc8cd9604a65e63a2f2223e01
Clayton Jensen <clayton.jensen2002@gmail.com>
2019-10-28 08:41:44 -0500

    Evil Diamond Comments

--- assignments/evilDiamond.cpp
+++ assignments/evilDiamond.cpp
@@ -2,36 +2,37 @@
 // Programming 1
 // October 23rd, 2019
 // Diamond From Triangle
-// From code that creates a rightside-up triangle, create a rightside-down triangle to create a diamond.
+// From code that creates a triangle, edit it to flip the output and make a diamond.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int space = 15;
+    int totalSpace = 30;
 
     // BEGIN TOP HALF
-    for (int i = 0; i <= 10; i++)
+    for (int currentLine = 0; currentLine <= 10; currentLine++)
     {
-        // Print the spacing required
+        // Print the spacing required for the current line:
         // Once this loop is hit, it is fully ran until the exit condition is met.
-        // Space decrements on each run, making the total space less than the previous line.
-        for (int k = 0; k < space; k++)
+        // totalSpace decrements on each line, making totalSpace less than the previous line.
+        // currentSpace increments on each space character printed until it meets totalSpace.
+        for (int currentSpace = 0; currentSpace < totalSpace; currentSpace++)
         {
             cout << ' ';
         }
 
-        // Print the amount of stars required for a single line
+        // Print the stars required for the current line:
         // Once the spacing loop is exited, this loop is ran until the exit condition is met.
-        // 2 * i-1 keeps the number of stars as an odd number.
-        for (int j = 0; j < 2 * i - 1; j++)
+        // 2 * currentLine-1 keeps the number of characters as an odd number.
+        for (int currentChar = 0; currentChar < 2 * currentLine - 1; currentChar++)
         {
-            cout << '*';
+            cout << '.';
         }
 
         // Decrement the spacing for the next iteration of the spacing for-loop.
-        space--;
+        totalSpace--;
 
         // End the line
         cout << '\n';
@@ -39,26 +40,26 @@ int main()
     // END TOP HALF
 
     // BEGIN BOTTOM HALF
-    for (int i = 10; i >= 0; i--)
+    for (int currentLine = 10; currentLine >= 0; currentLine--)
     {
-        // Print the spacing required
+        // Print the spacing required for the current line:
         // Once this loop is hit, it is fully ran until the exit condition is met.
-        // Space increments on each run, making the total space more than the previous line.
-        for (int k = 0; k < space; k++)
+        // totalSpace increments on each run, making the total totalSpace more than the previous line.
+        for (int currenttotalSpace = 0; currenttotalSpace < totalSpace; currenttotalSpace++)
         {
             cout << ' ';
         }
 
-        // Print the amount of stars required for a single line
+        // Print the stars required for the current line:
         // Once the spacing loop is exited, this loop is ran until the exit condition is met.
-        // 2 * i + 1 keeps the number of stars an odd number but as an inversed form.
-        for (int j = 0; j < 2 * i + 1; j++)
+        // 2 * currentLine + 1 keeps the number of stars an odd number but as an inversed form of the one above.
+        for (int currentChar = 0; currentChar < 2 * currentLine + 1; currentChar++)
         {
-            cout << '*';
+            cout << ".";
         }
 
         // Increment the spacing for the next iteration of the spacing for-loop.
-        space++;
+        totalSpace++;
 
         // End the line
         cout << '\n';
