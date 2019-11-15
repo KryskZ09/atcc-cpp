50c56c9a2396fc5e68549e055e2ec98979d15998
Clayton Jensen <clayton.jensen2002@gmail.com>
2019-11-14 21:14:17 -0600

    TTT Win Conditions Fixed 2

--- assignments/tictactoe.cpp
+++ assignments/tictactoe.cpp
@@ -73,6 +73,10 @@ void checkWin(char player)
     {
         winOutput(player);
     }
+    else if (board[2][0] == board[2][1] && board[2][1] == board[2][2])
+    {
+        winOutput(player);
+    }
     else if (board[2][0] == board[2][1] && board[2][1] == board[2][2]) // Row 3, Column 1, 2, 3
     {
         winOutput(player);
